029ae29e62839e07effd6d20f7988484ce1e5538
Your Name <you@example.com>
2025-09-08 17:34:43 +0530

    vite modi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/productcard/',
+  base: '/doorin/', // 👈 must match your GitHub repo name
   plugins: [react()],
 });
